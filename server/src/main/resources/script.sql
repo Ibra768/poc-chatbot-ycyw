3bc0bb5a3bdcefcf6843422b19c2fb994583e782
Ibrahim <i.benfaiala@gmail.com>
2024-11-22 20:37:12 +0100

    update script.sql

--- server/src/main/resources/script.sql
+++ server/src/main/resources/script.sql
@@ -16,11 +16,184 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `agency`
+--
+
+DROP TABLE IF EXISTS `agency`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `agency` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `adress` text,
+  `zipcode` int DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `open_at` time DEFAULT NULL,
+  `close_at` time DEFAULT NULL,
+  `tel` varchar(10) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `agency`
+--
+
+LOCK TABLES `agency` WRITE;
+/*!40000 ALTER TABLE `agency` DISABLE KEYS */;
+/*!40000 ALTER TABLE `agency` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `booking`
+--
+
+DROP TABLE IF EXISTS `booking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `booking` (
+  `id` int NOT NULL,
+  `offer` int DEFAULT NULL,
+  `user` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `rental_offer_id_fk` (`offer`),
+  KEY `booking_user_username_fk` (`user`),
+  CONSTRAINT `booking_offer_id_fk` FOREIGN KEY (`offer`) REFERENCES `offer` (`id`),
+  CONSTRAINT `booking_user_username_fk` FOREIGN KEY (`user`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking`
+--
+
+LOCK TABLES `booking` WRITE;
+/*!40000 ALTER TABLE `booking` DISABLE KEYS */;
+/*!40000 ALTER TABLE `booking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `conversation`
+--
+
+DROP TABLE IF EXISTS `conversation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `conversation` (
+  `id` int NOT NULL,
+  `type` enum('Tchat','Vidéo','Formulaire de contact') DEFAULT NULL,
+  `customer` varchar(255) DEFAULT NULL,
+  `agent` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `conversation_user_username_fk` (`agent`),
+  KEY `conversation_user_username_fk_2` (`customer`),
+  CONSTRAINT `conversation_user_username_fk` FOREIGN KEY (`agent`) REFERENCES `user` (`username`),
+  CONSTRAINT `conversation_user_username_fk_2` FOREIGN KEY (`customer`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conversation`
+--
+
+LOCK TABLES `conversation` WRITE;
+/*!40000 ALTER TABLE `conversation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `conversation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `faq`
+--
+
+DROP TABLE IF EXISTS `faq`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `faq` (
+  `question` text,
+  `answer` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `faq`
+--
+
+LOCK TABLES `faq` WRITE;
+/*!40000 ALTER TABLE `faq` DISABLE KEYS */;
+/*!40000 ALTER TABLE `faq` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `message` (
+  `id` int DEFAULT NULL,
+  `conversation` int DEFAULT NULL,
+  `sent_by` varchar(255) DEFAULT NULL,
+  `content` text,
+  KEY `message_conversation_id_fk` (`conversation`),
+  KEY `message_user_username_fk` (`sent_by`),
+  CONSTRAINT `message_conversation_id_fk` FOREIGN KEY (`conversation`) REFERENCES `conversation` (`id`),
+  CONSTRAINT `message_user_username_fk` FOREIGN KEY (`sent_by`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `offer`
+--
+
+DROP TABLE IF EXISTS `offer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `offer` (
+  `id` int NOT NULL,
+  `agency` int DEFAULT NULL,
+  `from` varchar(255) DEFAULT NULL,
+  `to` varchar(255) DEFAULT NULL,
+  `start` datetime DEFAULT NULL,
+  `end` datetime DEFAULT NULL,
+  `vehicle` int DEFAULT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `offer_agency_id_fk` (`agency`),
+  KEY `offer_vehicle_id_fk` (`vehicle`),
+  CONSTRAINT `offer_agency_id_fk` FOREIGN KEY (`agency`) REFERENCES `agency` (`id`),
+  CONSTRAINT `offer_vehicle_id_fk` FOREIGN KEY (`vehicle`) REFERENCES `vehicle` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `offer`
+--
+
+LOCK TABLES `offer` WRITE;
+/*!40000 ALTER TABLE `offer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `offer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
-CREATE DATABASE IF NOT EXISTS poc_chatbot;
-USE poc_chatbot;
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -29,6 +202,7 @@ CREATE TABLE `user` (
   `last_name` varchar(255) DEFAULT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `birth_date` date DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `adress` text,
@@ -46,8 +220,165 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('Lea76','Lefrançois','Lea','1993-10-15','lea76@gmail.com','$2a$10$ybp/lOzgRCewzox6lOcnz.0XQqlXEpJsl/p0XPkUkUCMEAtLqz90S',NULL,NULL,NULL,'2024-10-27 19:21:41','2024-10-27 19:31:39'),('Micka27','Dubois','Mickael','1983-10-13','mickael.dubois@gmail.com','$2a$10$ybp/lOzgRCewzox6lOcnz.0XQqlXEpJsl/p0XPkUkUCMEAtLqz90S',NULL,NULL,NULL,'2024-10-27 19:21:41','2024-10-27 19:31:39');
+INSERT INTO `user` VALUES ('Lea76','Lefrançois','Lea','1993-10-15',NULL,'lea76@gmail.com','$2a$10$ybp/lOzgRCewzox6lOcnz.0XQqlXEpJsl/p0XPkUkUCMEAtLqz90S',NULL,NULL,NULL,'2024-10-27 19:21:41','2024-10-27 19:31:39'),('Micka27','Dubois','Mickael','1983-10-13',NULL,'mickael.dubois@gmail.com','$2a$10$ybp/lOzgRCewzox6lOcnz.0XQqlXEpJsl/p0XPkUkUCMEAtLqz90S',NULL,NULL,NULL,'2024-10-27 19:21:41','2024-10-27 19:31:39');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle`
+--
+
+DROP TABLE IF EXISTS `vehicle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vehicle` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category` varchar(1) DEFAULT NULL,
+  `type` varchar(1) DEFAULT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `year` int DEFAULT NULL,
+  `transmission_drive` varchar(1) DEFAULT NULL,
+  `nb_seat` int DEFAULT NULL,
+  `nb_door` int DEFAULT NULL,
+  `fuel_ac` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `vehicle_vehicle_category_id_fk` (`category`),
+  KEY `vehicle_vehicle_type_id_fk` (`type`),
+  KEY `vehicle_vehicle_transmission_id_fk` (`transmission_drive`),
+  KEY `vehicle_vehicle_fuel_ac_id_fk` (`fuel_ac`),
+  CONSTRAINT `vehicle_vehicle_category_id_fk` FOREIGN KEY (`category`) REFERENCES `vehicle_category` (`id`),
+  CONSTRAINT `vehicle_vehicle_fuel_ac_id_fk` FOREIGN KEY (`fuel_ac`) REFERENCES `vehicle_fuel_ac` (`id`),
+  CONSTRAINT `vehicle_vehicle_transmission_id_fk` FOREIGN KEY (`transmission_drive`) REFERENCES `vehicle_transmission_drive` (`id`),
+  CONSTRAINT `vehicle_vehicle_type_id_fk` FOREIGN KEY (`type`) REFERENCES `vehicle_type` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+LOCK TABLES `vehicle` WRITE;
+/*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle_category`
+--
+
+DROP TABLE IF EXISTS `vehicle_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vehicle_category` (
+  `id` varchar(1) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle_category`
+--
+
+LOCK TABLES `vehicle_category` WRITE;
+/*!40000 ALTER TABLE `vehicle_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle_fuel_ac`
+--
+
+DROP TABLE IF EXISTS `vehicle_fuel_ac`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vehicle_fuel_ac` (
+  `id` varchar(1) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle_fuel_ac`
+--
+
+LOCK TABLES `vehicle_fuel_ac` WRITE;
+/*!40000 ALTER TABLE `vehicle_fuel_ac` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_fuel_ac` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle_transmission_drive`
+--
+
+DROP TABLE IF EXISTS `vehicle_transmission_drive`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vehicle_transmission_drive` (
+  `id` varchar(1) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle_transmission_drive`
+--
+
+LOCK TABLES `vehicle_transmission_drive` WRITE;
+/*!40000 ALTER TABLE `vehicle_transmission_drive` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_transmission_drive` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle_type`
+--
+
+DROP TABLE IF EXISTS `vehicle_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vehicle_type` (
+  `id` varchar(1) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle_type`
+--
+
+LOCK TABLES `vehicle_type` WRITE;
+/*!40000 ALTER TABLE `vehicle_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `video`
+--
+
+DROP TABLE IF EXISTS `video`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `video` (
+  `id` int NOT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `conversation` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `video_conversation_id_fk` (`conversation`),
+  CONSTRAINT `video_conversation_id_fk` FOREIGN KEY (`conversation`) REFERENCES `conversation` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `video`
+--
+
+LOCK TABLES `video` WRITE;
+/*!40000 ALTER TABLE `video` DISABLE KEYS */;
+/*!40000 ALTER TABLE `video` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -59,4 +390,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-10 20:37:04
+-- Dump completed on 2024-11-22 20:35:34
